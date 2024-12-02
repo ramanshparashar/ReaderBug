8451fe3963d180c308ceb9826abe728905fd33fe
ramanshparashar <ramansh2.0143@gmail.com>
2024-12-03 00:39:27 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import router from './router/router.jsx'
+import { store } from './redux/store.js'
+import { Provider } from 'react-redux'
+import 'sweetalert2/dist/sweetalert2.js'
+
+
+createRoot(document.getElementById('root')).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>,
+)
